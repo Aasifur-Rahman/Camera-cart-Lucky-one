5998a30185341d4a2ea83f276ae5f637c086418d
AsifurRahman66544 <93776889+AsifurRahman66544@users.noreply.github.com>
2022-03-26 15:26:37 +0600

    adding cart

--- src/Components/product/product.js
+++ src/Components/product/product.js
@@ -2,11 +2,20 @@ import React from 'react';
 import './Product.css';
 
 const Product = (props) => {
-    const { photo, name, id, price } = props.product;
+    const { photo, name, price } = props.product;
+    const handleAddToCart = () => {
+        console.log('clicked')
+    }
     return (
-        <div className='product'>
+        <div className='product container'>
             <img src={photo} alt="" />
-            <p className='product-name'>{name}</p>
+            <div className='product-info'>
+                <p className='product-name'>{name}</p>
+                <p>Price: {price} </p>
+            </div>
+            <button className='btn-cart '>
+                <p>Add to Cart</p>
+            </button>
         </div>
     );
 };
